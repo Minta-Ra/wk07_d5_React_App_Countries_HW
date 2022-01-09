f6fc2a4fb38a98cba91ea9833db7acf8cb9abe0e
Raminta Razauskaite <r.razauskaite@gmail.com>
2022-01-09 11:10:33 +0000

    API set and working

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import './App.css';
 import CountryContainer from './containers/CountryContainer';
 
 function App() {
+
+  const links = [
+    {name: "All Countries", url: "https://restcountries.com/v3.1/all"}
+  ];
+
   return (
-    <h1>Working</h1>
+    <CountryContainer links={links}/>
   );
-}
+};
 
 export default App;
--- src/containers/CountryContainer.js
+++ src/containers/CountryContainer.js
@@ -1,13 +1,25 @@
 import React, {useEffect, useState} from 'react';
 
-const CountryContainer = () => {
+const CountryContainer = ({links}) => {
 
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        loadCountries(links[0].url)
+    },[]);
+
+    const loadCountries = (url) => {
+        fetch(url)
+        .then(response => response.json())
+        .then(countriesList => setCountries(countriesList[0])) //.name.common
+        .catch(error => console.error);
+    };
 
 
 
     return (
         <h2>CountryContainer</h2>
-    )
+    );
 };
 
 export default CountryContainer;
